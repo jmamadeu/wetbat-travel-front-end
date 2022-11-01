85e5944973123bb0edb8cb392421457fc8b2cdfa
jmamadeu <jmamadeu2000@gmail.com>
2022-11-02 00:02:53 +0100

    feat: add api service

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Header } from "./components/header";
 import HomePage from "./pages/home/home";
+import { queryClient } from "./services/api/query-client";
 
 function App() {
   return (
-   <HomePage />
+    <QueryClientProvider client={queryClient}>
+      <Header />
+      <HomePage />
+    </QueryClientProvider> 
   );
 }
 
